310e4e539e0b63a4b183515d76435abbe46e77e5
Yonit <yonit.m@campus.technion.ac.il>
2023-07-11 09:06:20 +0300

    Corrected test for unique defect numbering and saving accordingly. Changed so only loads local OP if there are defects in the frame.

--- manualDefectAnalysis/extractAllDefects.m
+++ manualDefectAnalysis/extractAllDefects.m
@@ -1,7 +1,6 @@
 function [] = extractAllDefects(k, dirLocalOP, thisFileImNameBase,gTruth,dirDataDefect,useCenter)
 % Runs over all types of defects and extracts information from Ground Truth
 % Labeler format.
- cd(dirLocalOP); load(thisFileImNameBase); % load the localOP to find the defect location
 %     
 %     thisImWithDefect = thisIm; % this is the new image we will add annotations on
 %     % check if the image is a side by side image or single image
@@ -17,6 +16,7 @@ function [] = extractAllDefects(k, dirLocalOP, thisFileImNameBase,gTruth,dirData
     ind = find(contains(gTruth.DataSource.Source,thisFileImNameBase));
     
     if or(or( size(gTruth.LabelData.h{ind})>0, size(gTruth.LabelData.mh{ind})>0),size(gTruth.LabelData.o{ind})>0) % if we have any defects in thisFrame defects
+         cd(dirLocalOP); load(thisFileImNameBase); % load the localOP to find the defect location
         if size(gTruth.LabelData.h{ind})>0 % read in all the half defects
             for j=1: length(gTruth.LabelData.h{ind}),
                 numDefects(k) = numDefects(k)+1;
@@ -65,10 +65,15 @@ function [] = extractAllDefects(k, dirLocalOP, thisFileImNameBase,gTruth,dirData
     % defectID and save it as a structure "defect"
     defectIDs = [defectList.ID]; % this contains the ID of all the defects found in this frame
     defectTypes = [defectList.type];
-    for i=1:length(defectList),
-        if length(unique(defectIDs))<length(defectIDs)&&length(unique(defectTypes))<length(defectTypes) % if there are multiple defects with the same ID- display warning
-            display (['Defects in frame ',num2str(k), 'do not have unique IDs'])
-            defect(defectIDs(i))=defectList(i); % the structure defect now contains all the defect information so that defect(n) is the defect with ID n
+    uniqueTypes = unique(defectTypes);
+
+    for i=1:length(defectList)
+        for j=1:length(uniqueTypes)
+            % PUT HERE CORRECT CHECK OF UNIQUE IDS AND SUB INTO DEFECT STRUCTURE 
+            theseIDs = defectIDs(defectTypes==uniqueTypes(j));
+            if length(theseIDs)>length(unique(theseIDs))
+                display (['Defects in frame ',num2str(k), 'do not have unique IDs'])
+            end
         end
         defect(i)=defectList(i); % the structure defect now contains all the defect information so that defect(n) is the defect with ID n
     end
